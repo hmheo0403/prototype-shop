4a99b4c2f0c9a6ef96b7913cabc597ab4f75f84c
Hong Heo <hmheo0403@gmail.com>
2023-02-12 09:58:42 +0900

    work in progress

---
+++ src/components/Orders.jsx
@@ -0,0 +1,12 @@
+// components/Footer.jsx
+
+export default function Orders() {
+    return (
+        <aside>
+            <div className = "empty"> 
+            <div className = "title"> You don't have any orders</div>
+            <div className = "subtitle">Clieck on + to an order</div>
+            </div>
+        </aside>
+    );
+}
